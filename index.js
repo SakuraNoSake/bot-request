5842e39fec57a889b1852daf3a35c82df2bb41f6
SakuraNoSake <rus.nechaev01@mail.ru>
2024-02-02 02:03:25 +0300

    add picture

--- index.js
+++ index.js
@@ -8,6 +8,39 @@ const bot = new Telegraf(token);
 
 bot.start((ctx) => ctx.reply('Привет! Отправьте мне ваше предложение.'));
 
+// Middleware для обработки изображений
+bot.on('photo', async (ctx) => {
+    const userMessage = 'Изображение';
+
+    const forwardedMsg = await ctx.telegram.forwardMessage(adminId, ctx.message.chat.id, ctx.message.message_id);
+
+    const adminKeyboard = {
+        inline_keyboard: [
+            [{ text: 'Принять', callback_data: `accept_${forwardedMsg.message_id}` }],
+            [{ text: 'Отклонить', callback_data: `reject_${forwardedMsg.message_id}` }],
+        ],
+    };
+
+    const userNotification = '✅Ваше изображение отправлено на рассмотрение администратору';
+
+    const photo = ctx.message.photo ? ctx.message.photo[ctx.message.photo.length - 1] : null;
+    if (photo) {
+        // Отправляем фото в ответ пользователю
+        await ctx.telegram.sendPhoto(ctx.message.chat.id, photo.file_id, {
+            caption: userNotification,
+        });
+
+        // Отправляем фото в администратору
+        await ctx.telegram.sendPhoto(adminId, photo.file_id, {
+            caption: `\n${userMessage}`,
+            reply_markup: adminKeyboard,
+        });
+    } else {
+        // Если фотографии нет, отправляем только уведомление
+        await ctx.telegram.sendMessage(ctx.message.chat.id, 'Вы не отправили изображение.');
+    }
+});
+
 bot.on('text', async (ctx) => {
     const userMessage = ctx.message.text;
 
@@ -20,7 +53,7 @@ bot.on('text', async (ctx) => {
         ],
     };
 
-    const userNotification = 'Ваше предложение отправлено на рассмотрение администратору✅';
+    const userNotification = '✅Ваше предложение отправлено на рассмотрение администратору';
     await ctx.telegram.sendMessage(ctx.message.chat.id, userNotification);
 
     await ctx.telegram.sendMessage(adminId, `\n${userMessage}`, { reply_markup: adminKeyboard });
@@ -29,7 +62,18 @@ bot.on('text', async (ctx) => {
 bot.action(/accept_/, async (ctx) => {
     const messageId = ctx.callbackQuery.message.message_id;
     ctx.answerCbQuery('Предложение принято');
-    await ctx.telegram.sendMessage(channelId, `\n${ctx.callbackQuery.message.text}`);
+
+    const photo = ctx.callbackQuery.message.photo ? ctx.callbackQuery.message.photo[ctx.callbackQuery.message.photo.length - 1] : null;
+    if (photo) {
+        // Отправляем фото в канал
+        await ctx.telegram.sendPhoto(channelId, photo.file_id, {
+            caption: `✅Пикча из предложки`,
+        });
+    }else {
+        await ctx.telegram.sendMessage(channelId, `\n${ctx.callbackQuery.message.text}`);
+    }
+
+    // Удаляем клавиатуру у админского сообщения
     await ctx.telegram.editMessageReplyMarkup(adminId, messageId, null);
 });
 
